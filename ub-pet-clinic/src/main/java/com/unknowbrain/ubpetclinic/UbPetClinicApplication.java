6c48d7e5e7212f6ff5f0dcc07cc492a2aa408293
Kartik <kartik15nov@gmail.com>
2020-01-20 10:46:03 +0530

    Initial cut of Pet Clinic Project.

---
+++ ub-pet-clinic/src/main/java/com/unknowbrain/ubpetclinic/UbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.unknowbrain.ubpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbPetClinicApplication.class, args);
+	}
+
+}
---
+++ ub-pet-clinic/src/test/java/com/unknowbrain/ubpetclinic/UbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknowbrain.ubpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
