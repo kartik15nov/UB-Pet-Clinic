a5ef6db06c100707b3467ed1b9e9452def47f1e1
Kartik <kartik15nov@gmail.com>
2020-01-26 17:16:28 +0530

    refactoring to common crud interface. closes #3

--- ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/VetService.java
+++ ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/VetService.java
@@ -1,14 +1,6 @@
 package com.unknowbrain.ubpetclinic.services;
 
-import com.unknowbrain.ubpetclinic.model.Owner;
 import com.unknowbrain.ubpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Set<Vet> findAll();
-
-    Owner save(Vet vet);
+public interface VetService extends CrudService<Vet, Long> {
 }
