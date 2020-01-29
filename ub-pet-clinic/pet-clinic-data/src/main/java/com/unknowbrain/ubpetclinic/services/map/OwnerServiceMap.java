992137827a733dbba7da73a6c2e55fc86293a47b
Kartik <kartik15nov@gmail.com>
2020-01-29 10:14:50 +0530

    Added spring configuration for services. closes #12

--- ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/map/OwnerServiceMap.java
+++ ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.unknowbrain.ubpetclinic.services.map;
 
 import com.unknowbrain.ubpetclinic.model.Owner;
 import com.unknowbrain.ubpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
--- ub-pet-clinic/pet-clinic-web/src/main/java/com/unknowbrain/ubpetclinic/bootstrap/DataLoader.java
+++ ub-pet-clinic/pet-clinic-web/src/main/java/com/unknowbrain/ubpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.unknowbrain.ubpetclinic.model.Owner;
 import com.unknowbrain.ubpetclinic.model.Vet;
 import com.unknowbrain.ubpetclinic.services.OwnerService;
 import com.unknowbrain.ubpetclinic.services.VetService;
-import com.unknowbrain.ubpetclinic.services.map.OwnerServiceMap;
-import com.unknowbrain.ubpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        this.ownerService = new OwnerServiceMap();
-        this.vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
