a21733d9eb7a0f8a2d8863cc20c9faa18cad5abf
Kartik <kartik15nov@gmail.com>
2020-01-30 00:47:44 +0530

    Changed the AbstractMapService save() implementation as same as Crud service save, provided default id with next id generator..

--- ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/map/AbstractMapService.java
+++ ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.unknowbrain.ubpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.unknowbrain.ubpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     T findById(ID id) {
         return map.get(id);
@@ -16,8 +15,13 @@ public abstract class AbstractMapService<T, ID> {
         return new HashSet<>(map.values());
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if (object != null) {
+            if (object.getId() == null)
+                object.setId(getNextId());
+            map.put(object.getId(), object);
+        }
 
         return object;
     }
@@ -28,5 +32,17 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (Exception e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
