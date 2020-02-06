da174d748318b861f3f9fa6cc9a8d11e3c1e2d19
Kartik <kartik15nov@gmail.com>
2020-02-06 11:06:39 +0530

    Implemented repositories. Closes #41

---
+++ ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,7 @@
+package com.unknowbrain.ubpetclinic.repositories;
+
+import com.unknowbrain.ubpetclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+}
---
+++ ub-pet-clinic/pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/repositories/VisitRepository.java
@@ -0,0 +1,7 @@
+package com.unknowbrain.ubpetclinic.repositories;
+
+import com.unknowbrain.ubpetclinic.model.Visit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitRepository extends CrudRepository<Visit, Long> {
+}
