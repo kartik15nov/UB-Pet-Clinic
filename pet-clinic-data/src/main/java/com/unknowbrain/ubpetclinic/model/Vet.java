28964e4fe7134ea3d120a2f60b7c89eb0c1314b4
Don <kartik15nov@gmail.com>
2020-04-18 19:14:28 +0530

    Added list view for Vets. Closes #108

--- pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/unknowbrain/ubpetclinic/model/Vet.java
@@ -3,8 +3,8 @@ package com.unknowbrain.ubpetclinic.model;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,5 +19,5 @@ public class Vet extends Person {
     @JoinTable(name = "vet_specialties",
             joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
-    private Set<Specialty> specialties = new HashSet<>();
+    private List<Specialty> specialties = new ArrayList<>();
 }
